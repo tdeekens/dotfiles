51f394e72e8aaeef2659a93e48b5277b1048c950
Tobias Deekens <nerd@tdeekens.name>
2014-08-10 19:17:28 +0200

    Updates umsicht plugin.

--- .oh-my-zsh/plugins/umsicht/umsicht.plugin.zsh
+++ .oh-my-zsh/plugins/umsicht/umsicht.plugin.zsh
@@ -1,22 +1,22 @@
 function um() {
-   local action=$1; project=$2; option=$3; project_path=~/Development/
+   local action=$1; project=$2; option=$3; project_path=~/Development/;
 
    if [[ $action = "dev" ]]; then
-      echo "Lade Projekt '$project' in den Dev-Bereich.";
+      echo "Loading project: '$project' into development area.";
 
       cd "$project_path$project";
 
       aacm=$(git add --all && git commit -m "Automatic push to dev/pre");
       pom=$(git push origin master);
    elif [[ $action = "pre" ]]; then
-      echo "Lade Projekt '$project' in den Pre-Bereich.";
+      echo "Loading project: '$project' into preview area.";
 
       cd "$project_path$project";
 
       aacm=$(git add --all && git commit -m "Automatic push to dev/pre");
       pom=$(git push origin preview && git push origin master);
    elif [[ $action = "push" ]]; then
-      echo "Lade Projekt '$project' in den Dev-Bereich und pushe es.";
+      echo "Loading project: '$project' into development area and pushing it live.";
 
       cd "$project_path$project";
 
@@ -24,45 +24,58 @@ function um() {
       pom=$(git push origin master);
       push=$(git-ftp push);
    elif [[ $action = "down" ]]; then
-      echo "Lade Projekt '$project' lokal herunter.";
+      echo "Downloading project: '$project' locally.";
 
       cd "$project_path";
 
       clone=$(git clone ssh://um.dev/git/$project.git);
+      open=$(atom project_path);
    elif [[ $action = "update" ]]; then
-      echo "Update Projekt '$project' lokal...";
+      echo "Updating project: '$project' locally.";
 
       cd "$project_path$project";
 
       pull=$(git pull origin master);
+      open=$(atom project_path);
    elif [[ $action = "init" ]]; then
-      echo "Initiiere Projekt '$project' auf Produktionsserver.";
+      echo "Initializing project: '$project' on live server.";
+
+      sshScript=$(ssh um.dev 'sh /www/htdocs/v080275/scripts/stage-project.sh' $project dev);
 
       cd "$project_path$project";
 
       aacm=$(git add --all && git commit -m "Automatic init to dev/pre and production");
       pom=$(git push origin master);
       push=$(git-ftp init);
+   elif [[ $action = "new" ]]; then
+      echo "Creaing new project with name: '$project' and downloading it.";
+
+      sshScript=$(ssh um.dev 'sh /www/htdocs/v080275/scripts/bootstrap-project.sh' $project);
+
+      cd "$project_path";
+
+      clone=$(git clone ssh://um.dev/git/$project.git);
+      open=$(atom project_path);
    elif [[ $action = "config" ]]; then
       cd "$project_path$project";
 
       config=$(git config git-ftp.user $3 && git config git-ftp.password $4 && git config git-ftp.url $5);
 
-      echo "Zugangsdaten für $2 gesetzt mit Benutzer: $3, Passwort: $4 und FTP: $5\nInitiales pushen jetzt mit: um init $2";
+      echo "Config for $2 set with username: $3, password: $4 und url: $5\nInitialize project with: um init $2";
    elif [[ $action = "update-all" ]]; then
-      echo "Update *alle* Projekte lokal...";
+      echo "Updating *all* local projects.";
 
       cd "$project_path";
 
       for project_dir in $project_path*
       do
-         echo "Update Projekt '$project_dir' lokal.";
+         echo "Updating project '$project_dir' locally.";
 
          cd "$project_dir";
          pull=$(git pull origin master);
       done
    elif [[ $action = "work" ]]; then
-      echo "Setze Projekt '$project' zur Arbeit an '$option' Version auf...";
+      echo "Setting up project '$project' to work on '$option' version.";
 
       cd "$project_path$project";
 
@@ -74,16 +87,22 @@ function um() {
          co=$(git checkout master);
       fi
    elif [[ $action = "reset" ]]; then
-      echo "Setze Projekt '$project' zurück!";
+      echo "Removing and resetting '$project'!";
 
       rm -rf "$project_path$project";
 
       cd "$project_path";
 
       clone=$(git clone ssh://um.dev/git/$project.git);
+   elif [[ $action = "revert" ]]; then
+      echo "Reverting last made changes on: '$project'!";
+
+      cd "$project_path$project";
+
+      checkout=$(git checkout -f);
    elif [[ $action = "browse" ]]; then
-      open "http://tdeekens.local/$project";
+      open "http://localhost/$project";
    else
-      echo "Befehl '$action' unbekannt. Bekannte Befehlt z.B.: dev, push, down oder update. Zum Beispiel: 'um push umsicht'";
+      echo "Command '$action' unknown. Known commands e.g.: dev, push, down oder update. E.g.: 'um push umsicht'";
    fi
 }
