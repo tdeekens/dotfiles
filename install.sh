a275afae473547356f635696ce3e1fdb9729bc89
tobi <tobi@brandwatch.com>
2016-11-21 23:46:32 +0100

    Add install.sh to run brew through {Brew,Cask}file.

---
+++ install.sh
@@ -0,0 +1,9 @@
+set -e
+
+# Run Homebrew through the Brewfile
+echo "› brew bundle .Brewfile"
+brew bundle .Brewfile
+
+# Run Homebrew through the Caskfile
+echo "› brew bundle .Caskfile"
+brew bundle .Caskfile
