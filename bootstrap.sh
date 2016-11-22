d115261049b5fae8c4d3811b9c4f9e18bc9244d7
tobi <tobi@brandwatch.com>
2016-11-22 01:00:02 +0100

    Add new bootstrap script.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,23 +1,165 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "${BASH_SOURCE}")";
+DOTFILES_ROOT=$(pwd -P)
+DOTFILES=(
+.aliases
+.bash_prompt
+.bashrc
+.curlrc
+.editorconfig
+.exports
+.functions
+.gitattributes
+.gitconfig
+.gitignore
+.inputrc
+.oh-my-zsh/plugins/umsicht/umsicht.plugin.zsh
+.oh-my-zsh/themes/tdeekens.zsh-theme
+.osx
+.vim/colors/hybrid.vim
+.vimrc
+.wgetrc
+.zshrc)
 
-git pull origin master;
+set -e
 
-function doIt() {
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~;
-	source ~/.bash_profile;
+echo ''
+
+info () {
+  printf "\r  [ \033[00;34m..\033[0m ] $1\n"
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
-else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
-fi;
+user () {
+  printf "\r  [ \033[0;33m??\033[0m ] $1\n"
+}
 
-unset doIt;
+success () {
+  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+}
+
+fail () {
+  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
+  echo ''
+  exit
+}
+
+setup_gitconfig () {
+  if ! [ -f git/gitconfig.local.symlink ]
+  then
+    info 'setup gitconfig'
+
+    git_credential='cache'
+    if [ "$(uname -s)" == "Darwin" ]
+    then
+      git_credential='osxkeychain'
+    fi
+
+    user ' - What is your github author name?'
+    read -e git_authorname
+    user ' - What is your github author email?'
+    read -e git_authoremail
+
+    sed -e "s/AUTHORNAME/$git_authorname/g" -e "s/AUTHOREMAIL/$git_authoremail/g" -e "s/GIT_CREDENTIAL_HELPER/$git_credential/g" git/gitconfig.local.symlink.example > git/gitconfig.local.symlink
+
+    success 'gitconfig'
+  fi
+}
+
+
+link_file () {
+  local src=$1 dst=$2
+
+  local overwrite= backup= skip=
+  local action=
+
+  if [ -f "$dst" -o -d "$dst" -o -L "$dst" ]
+  then
+
+    if [ "$overwrite_all" == "false" ] && [ "$skip_all" == "false" ]
+    then
+
+      local currentSrc="$(readlink $dst)"
+
+      if [ "$currentSrc" == "$src" ]
+      then
+
+        skip=true;
+
+      else
+
+        user "File already exists: $dst ($(basename "$src")), what do you want to do?\n\
+        [s]kip, [S]kip all, [o]verwrite, [O]verwrite all?"
+        read -n 1 action
+
+        case "$action" in
+          o )
+            overwrite=true;;
+          O )
+            overwrite_all=true;;
+          s )
+            skip=true;;
+          S )
+            skip_all=true;;
+          * )
+            ;;
+        esac
+
+      fi
+
+    fi
+
+    overwrite=${overwrite:-$overwrite_all}
+    skip=${skip:-$skip_all}
+
+    if [ "$overwrite" == "true" ]
+    then
+      rm -rf "$dst"
+      success "removed $dst"
+    fi
+
+    if [ "$skip" == "true" ]
+    then
+      success "skipped $src"
+    fi
+  fi
+
+  if [ "$skip" != "true" ]  # "false" or empty
+  then
+    ln -s "$1" "$2"
+    success "linked $1 to $2"
+  fi
+}
+
+install_dotfiles () {
+  info 'installing dotfiles'
+
+  local overwrite_all=false backup=false skip_all=false
+
+  user "Want to [b]ackup all .dotfiles?"
+  read -n 1 action
+
+  case "$action" in
+    b )
+      backup=true;;
+    * )
+      ;;
+  esac
+
+  if [ "$backup" == "true" ]
+  then
+    ARCHIVE=( "${DOTFILES[@]/#/$HOME/}" )
+    tar -cvzf $HOME/.dotfiles-$(date '+%y-%m-%d').tar.gz "${ARCHIVE[@]}" &>/dev/null
+    success "backed up all in .dotfiles-$(date '+%y-%m-%d').tar.gz"
+  fi
+
+  for src in $DOTFILES
+  do
+    dst="$HOME/$src"
+    link_file "$src" "$dst"
+  done
+}
+
+install_dotfiles
+
+echo ''
+echo '  All installed!'
