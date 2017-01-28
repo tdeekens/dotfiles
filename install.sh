27e4c223420bbdd699892c18540a69a451ac1366
tdeekens <nerd@tdeekens.name>
2017-01-28 21:18:44 +0100

    Fix {Brewk,Cask}file to newer syntax.

--- install.sh
+++ install.sh
@@ -2,11 +2,7 @@ set -e
 
 # Run Homebrew through the Brewfile
 echo "› brew bundle .Brewfile"
-brew bundle .Brewfile
-
-# Run Homebrew through the Caskfile
-echo "› brew bundle .Caskfile"
-brew bundle .Caskfile
+brew bundle
 
 # Run setting up osx settings
 echo "› ./.osx"
